2090b19886986cb7ef282a3a049c85eadc832c08
Hagen M?bius <hagen.moebius@googlemail.com>
2012-10-12 15:18:33 +0200

    Import.

---
+++ Test/Application.cs
@@ -0,0 +1,11 @@
+﻿namespace Test
+{
+    internal static class Application
+    {
+        internal static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.Run(new MainWindow());
+        }
+    }
+}
