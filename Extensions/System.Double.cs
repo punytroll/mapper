e0d1caff02b84a5039f6326f870043b1d4616912
Hagen M?bius <hagen.moebius@googlemail.com>
2012-10-16 12:24:31 +0200

    Added more extensions for double and single types. Will be used in upcoming commits.

--- Extensions/System.Double.cs
+++ Extensions/System.Double.cs
@@ -4,4 +4,12 @@
     {
         return Double - System.Math.Truncate(Double);
     }
+
+    /// <summary>
+    /// Rounds the Double value towards 0 and converts it to an Int32 value.
+    /// </summary>
+    public static System.Int32 GetTruncatedAsInt32(this System.Double Double)
+    {
+        return System.Convert.ToInt32(System.Math.Truncate(Double));
+    }
 }
