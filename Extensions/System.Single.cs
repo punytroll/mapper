68396d827a559bb4fe75c08bd48fc29f810c6968
Hagen Möbius <hagen.moebius@googlemail.com>
2012-10-16 12:24:31 +0200

    Added more extensions for double and single types. Will be used in upcoming commits.

---
+++ Extensions/System.Single.cs
@@ -0,0 +1,15 @@
+﻿public static partial class Extensions
+{
+    /// <summary>
+    /// Rounds the Single value towards 0 and converts it to an Int32 value.
+    /// </summary>
+    public static System.Int32 GetTruncatedAsInt32(this System.Single Single)
+    {
+        return System.Convert.ToInt32(System.Math.Truncate(Single));
+    }
+
+    public static System.Single GetFraction(this System.Single Single)
+    {
+        return Single - System.Convert.ToSingle(System.Math.Truncate(Single));
+    }
+}
