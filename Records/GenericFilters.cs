88187abec24ad14a438b37dbead339b81ad73046
Hagen M?bius <hagen.moebius@googlemail.com>
2012-11-16 14:33:31 +0100

    The application internal format of the GPS points is now the record framework. Added the frame work to the sources.

---
+++ Records/GenericFilters.cs
@@ -0,0 +1,38 @@
+﻿public static class Extensions
+{
+    public static void UpdateField<FieldType>(this Records.Records Records, System.String FieldName, System.Func<FieldType, FieldType> Action)
+    {
+        Records.Map(Record => Record.Update(FieldName, Action(Record.Get<FieldType>(FieldName))));
+    }
+
+    public static void AddField<ResultType, InputType1, InputType2>(this Records.Records Records, System.String ResultFieldName, System.String InputFieldName1, System.String InputFieldName2, System.Func<InputType1, InputType2, ResultType> Action)
+    {
+        Records.Map(Record => Record.Add(ResultFieldName, Action(Record.Get<InputType1>(InputFieldName1), Record.Get<InputType2>(InputFieldName2))));
+    }
+
+    public static void AddField<ResultType, InputType>(this Records.Records Records, System.String ResultFieldName, System.String InputFieldName, System.Func<InputType, InputType, ResultType> Action)
+    {
+        if(Records.Count > 0)
+        {
+            Records.First.Add(ResultFieldName, default(ResultType));
+            Records.Map((Record1, Record2) => Record2.Add(ResultFieldName, Action(Record1.Get<InputType>(InputFieldName), Record2.Get<InputType>(InputFieldName))));
+        }
+    }
+
+    public static void AddField<ResultType, InputType>(this Records.Records Records, System.String ResultFieldName, System.String InputFieldName, System.Func<InputType, ResultType> Action)
+    {
+        Records.Map(Record => Record.Add(ResultFieldName, Action(Record.Get<InputType>(InputFieldName))));
+    }
+
+    public static AggregateType Aggregate<AggregateType>(this Records.Records Records, System.String InputFileName, System.Func<AggregateType, AggregateType, AggregateType> Aggregator)
+    {
+        if(Records.Count > 0)
+        {
+            return Records.Fold(Records.First.Get<AggregateType>(InputFileName), (Value, Record) => Aggregator(Value, Record.Get<AggregateType>(InputFileName)));
+        }
+        else
+        {
+            throw new System.InvalidOperationException();
+        }
+    }
+}
---
+++ Records/Record.cs
@@ -0,0 +1,113 @@
+﻿namespace Records
+{
+    public class Record
+    {
+        private readonly System.Collections.Generic.Dictionary<System.String, System.Object> _Fields;
+
+        public Record()
+        {
+            _Fields = new System.Collections.Generic.Dictionary<System.String, System.Object>();
+        }
+
+        public void Add<FieldType>(System.String FieldName, FieldType FieldValue)
+        {
+            if(_Fields.ContainsKey(FieldName) == false)
+            {
+                _Fields.Add(FieldName, FieldValue);
+            }
+            else
+            {
+                throw new System.ArgumentException("Fields '" + FieldName + "' already exists.");
+            }
+        }
+
+        public FieldType Get<FieldType>(System.String FieldName)
+        {
+            if(_Fields.ContainsKey(FieldName) == true)
+            {
+                if(_Fields[FieldName].GetType() == typeof(FieldType))
+                {
+                    return (FieldType)_Fields[FieldName];
+                }
+                else
+                {
+                    throw new System.InvalidCastException("Field '" + FieldName + "' does not contain a value of type '" + typeof(FieldType).FullName + "'.");
+                }
+            }
+            else
+            {
+                throw new System.ArgumentException("Field '" + FieldName + "' does not exist.");
+            }
+        }
+
+        public System.Boolean Has(System.String FieldName)
+        {
+            return _Fields.ContainsKey(FieldName);
+        }
+
+        public void Remove(System.String FieldName)
+        {
+            if(_Fields.ContainsKey(FieldName) == true)
+            {
+                _Fields.Remove(FieldName);
+            }
+            else
+            {
+                throw new System.ArgumentException("Field '" + FieldName + "' does not exist.");
+            }
+        }
+
+        public void Rename(System.String OldFieldName, System.String NewFieldName)
+        {
+            if(_Fields.ContainsKey(OldFieldName) == true)
+            {
+                if(_Fields.ContainsKey(NewFieldName) == false)
+                {
+                    var FieldValue = _Fields[OldFieldName];
+
+                    _Fields.Remove(OldFieldName);
+                    _Fields.Add(NewFieldName, FieldValue);
+                }
+                else
+                {
+                    throw new System.ArgumentException("Fields '" + NewFieldName + "' already exists.");
+                }
+            }
+            else
+            {
+                throw new System.ArgumentException("Field '" + OldFieldName + "' does not exist.");
+            }
+        }
+
+        public void Replace<FieldType>(System.String FieldName, FieldType FieldValue)
+        {
+            if(_Fields.ContainsKey(FieldName) == true)
+            {
+                _Fields[FieldName] = FieldValue;
+            }
+            else
+            {
+                throw new System.ArgumentException("Field '" + FieldName + "' does not exist.");
+            }
+        }
+
+        public void Update<FieldType>(System.String FieldName, FieldType FieldValue)
+        {
+            if(_Fields.ContainsKey(FieldName) == true)
+            {
+                if(typeof(FieldType) == _Fields[FieldName].GetType())
+                {
+                    _Fields[FieldName] = FieldValue;
+                }
+                else
+                {
+                    throw new System.ArgumentException("Field '" + FieldName + "' has not the type '" + typeof(FieldType).FullName + "'.");
+                }
+            }
+            else
+            {
+                throw new System.ArgumentException("Field '" + FieldName + "' does not exist.");
+            }
+        }
+    }
+}
