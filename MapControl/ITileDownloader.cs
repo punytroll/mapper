1f6d7f7849dfe264c7f32d3b4bca7567927f609f
Hagen M?bius <hagen.moebius@googlemail.com>
2013-05-08 11:15:29 +0200

    Work in progress.

---
+++ MapControl/ITileDownloader.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public interface ITileDownloader
+    {
+        System.Boolean SupportsTile(System.Windows.Forms.MapTile Tile);
+        void FetchTile(System.Windows.Forms.MapTile Tile);
+        System.String GetSetIdentifier();
+        System.Int32 GetTileSize();
+    }
+}
---
+++ MapControl/ImageMemoryCache.cs
@@ -0,0 +1,52 @@
+﻿namespace System
+{
+    public class ImageMemoryCache
+    {
+        private struct ImageIdentifier
+        {
+            System.Int32 X;
+            System.Int32 Y;
+            System.Int32 Zoom;
+
+            public ImageIdentifier(System.Int32 Zoom, System.Int32 X, System.Int32 Y)
+            {
+                this.X = X;
+                this.Y = Y;
+                this.Zoom = Zoom;
+            }
+        }
+
+        private readonly System.Collections.Generic.Dictionary<ImageIdentifier, System.Drawing.Image> _Cache;
+
+        public ImageMemoryCache()
+        {
+            _Cache = new System.Collections.Generic.Dictionary<ImageIdentifier, System.Drawing.Image>();
+        }
+
+        public System.Boolean HasImage(System.Int32 Zoom, System.Int32 X, System.Int32 Y)
+        {
+            return _Cache.ContainsKey(new ImageIdentifier(Zoom, X, Y)) == true;
+        }
+
+        public System.Drawing.Image GetImage(System.Int32 Zoom, System.Int32 X, System.Int32 Y)
+        {
+            var ImageIdentifier = new ImageIdentifier(Zoom, X, Y);
+
+            if(_Cache.ContainsKey(ImageIdentifier) == true)
+            {
+                return _Cache[ImageIdentifier];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void SetImage(System.Int32 Zoom, System.Int32 X, System.Int32 Y, System.Drawing.Image Image)
+        {
+            var ImageIdentifier = new ImageIdentifier(Zoom, X, Y);
+
+            _Cache[ImageIdentifier] = Image;
+        }
+    }
+}
