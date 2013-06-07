6c41a71f3118cac52b05c18305be734c25218f21
Hagen M?bius <hagen.moebius@googlemail.com>
2013-06-07 11:05:50 +0200

    Added rudimentary KML capabilities. The Mapper can now read kml-files which contain placemarks with LineStrings and display them as lines on the data map. Elevation is a GPX term, the correct term is altitude.

--- MapControl/DataMap.cs
+++ MapControl/DataMap.cs
@@ -1,14 +1,73 @@
-﻿namespace System.Windows.Forms
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace System.Windows.Forms
 {
     public class DataMap : System.Windows.Forms.Map
     {
+        public class Line
+        {
+            private System.Point _BeginGeoLocation;
+            private Color _Color;
+            private System.Point _EndGeoLocation;
+            private Object _Object;
+
+            public System.Point BeginGeoLocation
+            {
+                get
+                {
+                    return _BeginGeoLocation;
+                }
+                set
+                {
+                    _BeginGeoLocation = value;
+                }
+            }
+
+            public Color Color
+            {
+                get
+                {
+                    return _Color;
+                }
+                set
+                {
+                    _Color = value;
+                }
+            }
+
+            public System.Point EndGeoLocation
+            {
+                get
+                {
+                    return _EndGeoLocation;
+                }
+                set
+                {
+                    _EndGeoLocation = value;
+                }
+            }
+
+            public Object Object
+            {
+                get
+                {
+                    return _Object;
+                }
+                set
+                {
+                    _Object = value;
+                }
+            }
+        }
+
         public class Point
         {
-            private System.Drawing.Color _Color;
+            private Color _Color;
             private System.Point _GeoLocation;
-            private System.Object _Object;
+            private Object _Object;
 
-            public System.Drawing.Color Color
+            public Color Color
             {
                 get
                 {
@@ -32,7 +91,7 @@
                 }
             }
 
-            public System.Object Object
+            public Object Object
             {
                 get
                 {
@@ -45,9 +104,18 @@
             }
         }
 
-        private readonly System.Collections.Generic.List<Point> _Points;
+        private readonly List<Line> _Lines;
+        private readonly List<Point> _Points;
 
-        public System.Collections.Generic.List<Point> Points
+        public List<Line> Lines
+        {
+            get
+            {
+                return _Lines;
+            }
+        }
+
+        public List<Point> Points
         {
             get
             {
@@ -57,20 +125,30 @@
 
         public DataMap()
         {
-            _Points = new System.Collections.Generic.List<Point>();
+            _Lines = new List<Line>();
+            _Points = new List<Point>();
         }
 
         protected override void OnPaint(System.Windows.Forms.PaintEventArgs EventArguments)
         {
             base.OnPaint(EventArguments);
+            foreach(var Line in _Lines)
+            {
+                var BeginScreenLocation = GetScreenLocationFromGeoLocation(Line.BeginGeoLocation);
+                var EndScreenLocation = GetScreenLocationFromGeoLocation(Line.EndGeoLocation);
 
+                if(((BeginScreenLocation.X >= 0) && (BeginScreenLocation.Y >= 0) && (BeginScreenLocation.X <= Width) && (BeginScreenLocation.Y <= Height)) || ((EndScreenLocation.X >= 0) && (EndScreenLocation.Y >= 0) && (EndScreenLocation.X <= Width) && (EndScreenLocation.Y <= Height)))
+                {
+                    EventArguments.Graphics.DrawLine(new Pen(Line.Color, 5), BeginScreenLocation.X, BeginScreenLocation.Y, EndScreenLocation.X, EndScreenLocation.Y);
+                }
+            }
             foreach(var Point in _Points)
             {
                 var ScreenLocation = GetScreenLocationFromGeoLocation(Point.GeoLocation);
 
                 if((ScreenLocation.X >= 0) && (ScreenLocation.Y >= 0) && (ScreenLocation.X <= Width) && (ScreenLocation.Y <= Height))
                 {
-                    EventArguments.Graphics.FillRectangle(new System.Drawing.SolidBrush(Point.Color), ScreenLocation.X - 2, ScreenLocation.Y - 2, 5, 5);
+                    EventArguments.Graphics.FillRectangle(new SolidBrush(Point.Color), ScreenLocation.X - 2, ScreenLocation.Y - 2, 5, 5);
                 }
             }
         }
