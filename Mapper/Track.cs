05efed88c28f42d2dc6c3a5360c82f2c0d51db85
Hagen M?bius <hagen.moebius@googlemail.com>
2013-06-10 08:57:43 +0200

    Added a Track class which will hold all track specific data. Minor cleanups. Fixed a bug with displaying the geo coordinates under the mouse cursor.

---
+++ Mapper/Track.cs
@@ -0,0 +1,23 @@
+﻿namespace Mapper
+{
+    public class Track : Records.Records
+    {
+        public System.String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        private System.String _Name;
+
+        public Track()
+        {
+        }
+    }
+}
